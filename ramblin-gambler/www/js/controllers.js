ef7bd15c9f4557882144c5d8431f1ab86b8e7e81
Tyler Briskie <tylerbriskie@gmail.com>
2017-02-16 13:40:24 -0700

    showing active games by user ID

--- ramblin-gambler/www/js/controllers.js
+++ ramblin-gambler/www/js/controllers.js
@@ -107,13 +107,14 @@ angular.module('starter.controllers', [])
 .controller('gamesCtrl', function($scope, $http) {
     const vm = this;
     function getPendingGames () {
-      $http.get('http://localhost:3000/games_users').success(function(data){
+      $http.get('http://localhost:3000/games_users/pending').success(function(data){
         vm.pendingGames = data
       })
     };
     function getActiveGames () {
-      $http.get('http://localhost:3000/games_users_wagers').success(function(data){
+      $http.get('http://localhost:3000/games_users/active/'+ localStorage.user_id).success(function(data){
         vm.activeGames = data
+        console.log(data)
       })
     }
     getPendingGames();
@@ -209,19 +210,63 @@ angular.module('starter.controllers', [])
   }
 })
 
-.controller('gameCtrl', function($http, $scope, $stateParams) {
+.controller('gameCtrl', function($http, $geolocation, $scope, $stateParams) {
   const vm = this;
-  $http.get('http://localhost:3000/games/'+ $stateParams.id)
-    .then(data=>{
-      console.log(data)
-      vm.game_params = {
-        game_id: data.data.id,
-        creator_name: "Matt" ,
-        start_time: data.data.time,
-        game_type: data.data.type,
-      }
-    })
+  $geolocation.getCurrentPosition({
+    timeout: 60000
+    }).then(function(position) {
+       $scope.getDistance = function(lat1, long1, lat2, long2){
+         Number.prototype.toRad = function() {
+           return this * Math.PI / 180;
+         }
 
+         var R = 6371; // km
+         //has a problem with the .toRad() method below.
+         var x1 = lat2-lat1;
+         var dLat = x1.toRad();
+         var x2 = long2-long1;
+         var dLong = x2.toRad();
+         var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+                         Math.cos(lat1.toRad()) * Math.cos(lat2.toRad()) *
+                         Math.sin(dLong/2) * Math.sin(dLong/2);
+         var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+         var d = R * c;
+         var inMiles = (d/2)/4 + (d/2)
+         return inMiles
+       }
+        $scope.myPosition = position;
+        let myLocation = {
+          lat: $scope.myPosition.coords.latitude,
+          long: $scope.myPosition.coords.longitude
+        };
+
+        $http.get('http://localhost:3000/games/'+ $stateParams.id)
+          .then(data=>{
+            console.log(data)
+            vm.game_params = {
+              game_id: data.data.id,
+              creator_name: "Matt" ,
+              start_time: data.data.time,
+              game_type: data.data.type,
+              lat2: data.data.lat,
+              long2: data.data.long,
+            }
+
+            vm.theDistance = $scope.getDistance(myLocation.lat, myLocation.long, vm.game_params.lat2, vm.game_params.long2)
+            vm.distanceToHere = vm.theDistance.toFixed(2);
+            console.log(vm.distanceToHere);
+          })
+
+      });
+
+
+    $scope.joinGame = function(){
+      console.log("Joining game")
+      $http.put('http://localhost:3000/games/' + $stateParams.id, {
+        is_active:"active"
+      })
+      $http.post('http://localhost:3000/games/'+ $stateParams.id + '/user/' + localStorage.user_id)
+    }
   console.log("state params: ", $stateParams);
 })
 
